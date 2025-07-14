6194907b0b2dc098d6d25ab960f57f691e3c19c4
Troptop <87533146+NickIsADev@users.noreply.github.com>
2025-07-14 14:20:04 -0500

    "theme"

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const map = L.map('map').setView([0, 0], 2);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',
+    maxZoom: 19
 }).addTo(map);
 
 const aircraftLayer = L.layerGroup().addTo(map);
